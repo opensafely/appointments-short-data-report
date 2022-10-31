aec23d252bd7abfdbc6bae1f1c39375c056b6411
Iain Dillingham <iain@dillingham.me.uk>
2022-10-31 15:17:21 +0000

    Add `isoformat_date`

--- analysis/utils.py
+++ analysis/utils.py
@@ -11,3 +11,8 @@ round_to_seven = functools.partial(round_to_multiple, multiple=7)
 
 
 open_csv = functools.partial(open, newline="", encoding="utf-8")
+
+
+def isoformat_date(year, month="1", day="1"):
+    year, month, day = [int(x) for x in [year, month, day]]
+    return f"{year}-{month:02d}-{day:02d}"
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -6,3 +6,18 @@ from analysis import utils
 @pytest.mark.parametrize("value,rounded_value", [(6, 0), (7, 7)])
 def test_round_to_multiple(value, rounded_value):
     assert utils.round_to_seven(value) == rounded_value
+
+
+@pytest.mark.parametrize(
+    "args,isoformat_date",
+    [
+        (("2022",), "2022-01-01"),
+        (("2022", "9"), "2022-09-01"),
+        (("2022", "10"), "2022-10-01"),
+        (("2022", "9", "9"), "2022-09-09"),
+        (("2022", "10", "10"), "2022-10-10"),
+        (("9999", "99", "99"), "9999-99-99"),  # doesn't validate arguments
+    ],
+)
+def test_isoformat_date(args, isoformat_date):
+    assert utils.isoformat_date(*args) == isoformat_date
