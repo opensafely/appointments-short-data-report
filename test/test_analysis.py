a49304a6a2e7de051da85bf83ea0ea2427b567b2
Iain Dillingham <iain@dillingham.me.uk>
2022-11-03 17:39:04 +0000

    Test GT `multiple`
    We already test LT and ET `multiple`, so for completeness let's test GT.

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -3,7 +3,7 @@ import pytest
 from analysis import utils
 
 
-@pytest.mark.parametrize("value,rounded_value", [(6, 7), (7, 7)])
+@pytest.mark.parametrize("value,rounded_value", [(6, 7), (7, 7), (8, 7)])
 def test_round_to_multiple(value, rounded_value):
     assert utils.round_to_seven(value) == rounded_value
 
