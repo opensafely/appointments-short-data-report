c0329a3ca792e15532241dc763c50b8b10dffe93
Iain Dillingham <iain@dillingham.me.uk>
2022-11-07 20:36:30 +0000

    Remove `wrangle_num_rows_by_month`
    We moved rounding from this action in 86447bb, so this action serves
    only to join year-date parts. We can do this in the query, and remove
    `wrangle_num_rows_by_month`.

--- analysis/num_rows_by_month/query.sql
+++ analysis/num_rows_by_month/query.sql
@@ -1,28 +1,23 @@
 SELECT
     column_name,
-    year,
-    month,
+    date,
     COUNT(Appointment_ID) AS num_rows
 FROM (
     SELECT
         Appointment_ID,
         'BookedDate' AS column_name,
-        YEAR(BookedDate) AS year,
-        MONTH(BookedDate) AS month
+        DATEFROMPARTS(YEAR(BookedDate), MONTH(BookedDate), 1) AS date -- noqa:L016,L029
     FROM Appointment
     UNION ALL
     SELECT
         Appointment_ID,
         'StartDate' AS column_name,
-        YEAR(StartDate) AS year,
-        MONTH(StartDate) AS month
+        DATEFROMPARTS(YEAR(StartDate), MONTH(StartDate), 1) AS date -- noqa:L029
     FROM Appointment
 ) AS t
 GROUP BY
     column_name,
-    year,
-    month
+    date
 ORDER BY
     column_name,
-    year,
-    month
+    date
