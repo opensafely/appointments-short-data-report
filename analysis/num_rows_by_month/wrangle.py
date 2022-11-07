86447bb473332c1a3cc459a641b7b2bd73a15ce8
Iain Dillingham <iain@dillingham.me.uk>
2022-11-07 18:15:35 +0000

    Add `round_num_rows_by_month`
    We move rounding from the existing `wrangle_num_rows_by_month` action to
    a new `round_num_rows_by_month` action, increasing the sensitivity of
    the former.

--- analysis/num_rows_by_month/wrangle.py
+++ analysis/num_rows_by_month/wrangle.py
@@ -10,7 +10,9 @@ ACTION_OUTPUT_DIR = OUTPUT_DIR / "num_rows_by_month"
 def main():
     with contextlib.ExitStack() as s:
         f_in = s.enter_context(utils.open_csv(ACTION_OUTPUT_DIR / "rows.csv"))
-        f_out = s.enter_context(utils.open_csv(ACTION_OUTPUT_DIR / "results.csv", "w"))
+        f_out = s.enter_context(
+            utils.open_csv(ACTION_OUTPUT_DIR / "wrangled_rows.csv", "w")
+        )
 
         reader = csv.reader(f_in)
         writer = csv.writer(f_out)
@@ -20,12 +22,10 @@ def main():
 
         for row in reader:
             column_name, year, month, num_rows = row
-            num_rows = int(num_rows)
 
             date = utils.isoformat_date(year, month)
-            rounded_num_rows = utils.round_to_seven(num_rows)
 
-            writer.writerow([column_name, date, rounded_num_rows])
+            writer.writerow([column_name, date, num_rows])
 
 
 if __name__ == "__main__":
