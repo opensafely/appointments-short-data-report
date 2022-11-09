62af5a6a792ddb7e8c9e8fa4b3996d9948dfb998
Iain Dillingham <iain@dillingham.me.uk>
2022-11-09 17:15:37 +0000

    Return dates rather than datetimes
    To mitigate disclosure risk, we return dates rather than datetimes from
    the max/min query.

--- analysis/date_range/query.sql
+++ analysis/date_range/query.sql
@@ -1,35 +1,35 @@
 SELECT
     'BookedDate' AS column_name,
-    MAX(BookedDate) AS max_date,
-    MIN(BookedDate) AS min_date
+    CAST(MAX(BookedDate) AS DATE) AS max_date,
+    CAST(MIN(BookedDate) AS DATE) AS min_date
 FROM Appointment
 UNION ALL
 SELECT
     'StartDate' AS column_name,
-    MAX(StartDate) AS max_date,
-    MIN(StartDate) AS min_date
+    CAST(MAX(StartDate) AS DATE) AS max_date,
+    CAST(MIN(StartDate) AS DATE) AS min_date
 FROM Appointment
 UNION ALL
 SELECT
     'ArrivedDate' AS column_name,
-    MAX(ArrivedDate) AS max_date,
-    MIN(ArrivedDate) AS min_date
+    CAST(MAX(ArrivedDate) AS DATE) AS max_date,
+    CAST(MIN(ArrivedDate) AS DATE) AS min_date
 FROM Appointment
 UNION ALL
 SELECT
     'EndDate' AS column_name,
-    MAX(EndDate) AS max_date,
-    MIN(EndDate) AS min_date
+    CAST(MAX(EndDate) AS DATE) AS max_date,
+    CAST(MIN(EndDate) AS DATE) AS min_date
 FROM Appointment
 UNION ALL
 SELECT
     'FinishedDate' AS column_name,
-    MAX(FinishedDate) AS max_date,
-    MIN(FinishedDate) AS min_date
+    CAST(MAX(FinishedDate) AS DATE) AS max_date,
+    CAST(MIN(FinishedDate) AS DATE) AS min_date
 FROM Appointment
 UNION ALL
 SELECT
     'SeenDate' AS column_name,
-    MAX(SeenDate) AS max_date,
-    MIN(SeenDate) AS min_date
+    CAST(MAX(SeenDate) AS DATE) AS max_date,
+    CAST(MIN(SeenDate) AS DATE) AS min_date
 FROM Appointment
