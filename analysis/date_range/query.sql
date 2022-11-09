80c0b3c2306dc70b20399c66fbb18fc5643c1fb2
Iain Dillingham <iain@dillingham.me.uk>
2022-11-09 17:35:16 +0000

    Reorder min_date and max_date columns
    2da2a59 accomplished it in the notebook; here we accomplish it in the
    query. This allows us to remove a (very) small amount of logic from the
    notebook.

--- analysis/date_range/query.sql
+++ analysis/date_range/query.sql
@@ -1,35 +1,35 @@
 SELECT
     'BookedDate' AS column_name,
-    CAST(MAX(BookedDate) AS DATE) AS max_date,
-    CAST(MIN(BookedDate) AS DATE) AS min_date
+    CAST(MIN(BookedDate) AS DATE) AS min_date,
+    CAST(MAX(BookedDate) AS DATE) AS max_date
 FROM Appointment
 UNION ALL
 SELECT
     'StartDate' AS column_name,
-    CAST(MAX(StartDate) AS DATE) AS max_date,
-    CAST(MIN(StartDate) AS DATE) AS min_date
+    CAST(MIN(StartDate) AS DATE) AS min_date,
+    CAST(MAX(StartDate) AS DATE) AS max_date
 FROM Appointment
 UNION ALL
 SELECT
     'ArrivedDate' AS column_name,
-    CAST(MAX(ArrivedDate) AS DATE) AS max_date,
-    CAST(MIN(ArrivedDate) AS DATE) AS min_date
+    CAST(MIN(ArrivedDate) AS DATE) AS min_date,
+    CAST(MAX(ArrivedDate) AS DATE) AS max_date
 FROM Appointment
 UNION ALL
 SELECT
     'EndDate' AS column_name,
-    CAST(MAX(EndDate) AS DATE) AS max_date,
-    CAST(MIN(EndDate) AS DATE) AS min_date
+    CAST(MIN(EndDate) AS DATE) AS min_date,
+    CAST(MAX(EndDate) AS DATE) AS max_date
 FROM Appointment
 UNION ALL
 SELECT
     'FinishedDate' AS column_name,
-    CAST(MAX(FinishedDate) AS DATE) AS max_date,
-    CAST(MIN(FinishedDate) AS DATE) AS min_date
+    CAST(MIN(FinishedDate) AS DATE) AS min_date,
+    CAST(MAX(FinishedDate) AS DATE) AS max_date
 FROM Appointment
 UNION ALL
 SELECT
     'SeenDate' AS column_name,
-    CAST(MAX(SeenDate) AS DATE) AS max_date,
-    CAST(MIN(SeenDate) AS DATE) AS min_date
+    CAST(MIN(SeenDate) AS DATE) AS min_date,
+    CAST(MAX(SeenDate) AS DATE) AS max_date
 FROM Appointment
