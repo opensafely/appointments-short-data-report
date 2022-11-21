22bd7f2a29677fe0148e893fac9ce812e946f676
Iain Dillingham <iain@dillingham.me.uk>
2022-11-21 12:38:36 +0000

    Count distinct values by Status

--- analysis/distinct_values/query.sql
+++ analysis/distinct_values/query.sql
@@ -1,14 +1,14 @@
 SELECT
-    t.Organisation_ID,
+    t.Status,
     COUNT(*) AS num_distinct_values,
     SUM(t.num_values) AS num_values
 FROM (
     SELECT
-        Organisation_ID,
+        Status,
         Appointment_ID,
         COUNT(*) AS num_values
     FROM Appointment
-    GROUP BY Organisation_ID, Appointment_ID
+    GROUP BY Status, Appointment_ID
 ) AS t
-GROUP BY t.Organisation_ID
-ORDER BY t.Organisation_ID
+GROUP BY t.Status
+ORDER BY t.Status
