ce2f289fee246465e4030d436899ba41d216e355
Iain Dillingham <iain@dillingham.me.uk>
2022-11-22 19:41:43 +0000

    Count distinct values by Organisation_ID
    In effect, this commit reverts 22bd7f2 and 57760e7. However, it updates
    the notebook, retaining some information and removing the call to
    `pandas.read_csv`. This makes it easier to establish whether rows in the
    `Appointment` table represent distinct appointments in some cases but
    not others, as we need edit only the query.

--- analysis/distinct_values/query.sql
+++ analysis/distinct_values/query.sql
@@ -1,14 +1,14 @@
 SELECT
-    t.Status,
+    t.Organisation_ID,
     COUNT(*) AS num_distinct_values,
     SUM(t.num_values) AS num_values
 FROM (
     SELECT
-        Status,
+        Organisation_ID,
         Appointment_ID,
         COUNT(*) AS num_values
     FROM Appointment
-    GROUP BY Status, Appointment_ID
+    GROUP BY Organisation_ID, Appointment_ID
 ) AS t
-GROUP BY t.Status
-ORDER BY t.Status
+GROUP BY t.Organisation_ID
+ORDER BY t.Organisation_ID
